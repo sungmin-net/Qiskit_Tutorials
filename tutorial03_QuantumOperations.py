2ef90490f62ecec2a8beca719934f9c728e52981
Sungmin <lee@sungmin.net>
2021-06-24 13:24:19 +0900

    change tutorial 3

--- tutorial03_QuantumOperations.py
+++ tutorial03_QuantumOperations.py
@@ -1,5 +1,5 @@
  # https://qiskit.org/documentation/tutorials/circuits/3_summary_of_quantum_operations.html
-
+ 
 import matplotlib.pyplot as plt
 import numpy as np
 from math import pi
@@ -9,39 +9,69 @@ from qiskit.tools.visualization import circuit_drawer
 from qiskit.quantum_info import state_fidelity
 from qiskit import BasicAer
 
+from qiskit.visualization import plot_bloch_multivector # added
+ 
+# Single Qubit Quantum states :  |φ> = α|0> + β|1>
 backend = BasicAer.get_backend('unitary_simulator')
-
+  
 # Single Qubit Gates -----------------------------------------------
 q = QuantumRegister(1)
+qc = QuantumCircuit(q)
+plot_bloch_multivector(qc)
+plt.show()
 
 # U(unitary) gates
-qc = QuantumCircuit(q)
 qc.u3(pi / 2, pi / 2, pi / 2, q)
-qc.draw('mpl') # added 'mpl'
-plt.show() # added
 
+plot_bloch_multivector(qc)
+plt.show()
+
+
+'''
+The QuantumCircuit.u3 method is deprecated as of 0.16.0. It will be removed no earlier than 3 months
+after the release date. You should use QuantumCircuit.u instead, which acts identically. 
+Alternatively, you can decompose u3 in terms of QuantumCircuit.p and QuantumCircuit.sx: 
+u3(ϴ,φ,λ) = p(φ+π) sx p(ϴ+π) sx p(λ) (2 pulses on hardware).
+  qc.u3(pi / 2, pi / 2, pi / 2, q)
+'''
+qc.draw('mpl') # added 'mpl'
+#plt.show() # added
+  
 job = execute(qc, backend)
 print(job.result().get_unitary(qc, decimals = 3)) # print() added
 
 qc = QuantumCircuit(q)
+qc.draw('mpl')
+plt.show()
+
 qc.u2(pi / 2, pi / 2, q)
+'''
+The QuantumCircuit.u2 method is deprecated as of 0.16.0. It will be removed no earlier than 3 months
+after the release date. You can use the general 1-qubit gate QuantumCircuit.u instead: 
+u2(φ,λ) = u(π/2, φ, λ). Alternatively, you can decompose it interms of QuantumCircuit.p and
+QuantumCircuit.sx: u2(φ,λ) = p(π/2+φ) sx p(π/2+λ) (1 pulse on hardware).
+'''
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
-print(job.result().get_unitary(qc, decimals = 3)) # print() added
+print(job.result().get_unitary(qc, decimals = 3)) # print() added 
 
 qc = QuantumCircuit(q)
 qc.u1(pi/2, q)
-
+'''
+The QuantumCircuit.u1 method is deprecated as of 0.16.0. It will be removed no earlier than 3 months
+after the release date. You should use the QuantumCircuit.p method instead, which acts identically.
+qc.u1(pi/2, q)
+'''
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
-
+#plt.show() # added
+ 
 # I(identity) gate
 qc = QuantumCircuit(q)
 qc.id(q)
-qc.draw('mpl') # 'mpl' added
-plt.show() # added
+qc.draw('mpl') # 'mpl' added 
+#plt.show() # added
 
 job = execute(qc, backend)
 print(job.result().get_unitary(qc, decimals = 3))
@@ -51,8 +81,8 @@ print(job.result().get_unitary(qc, decimals = 3))
 
 qc = QuantumCircuit(q)
 qc.x(q)
-qc.draw('mpl') # 'mpl' added
-plt.show() # added
+qc.draw('mpl') # 'mpl' added 
+#plt.show() # added
 
 job = execute(qc, backend)
 print(job.result().get_unitary(qc, decimals = 3)) # print() added
@@ -61,7 +91,7 @@ print(job.result().get_unitary(qc, decimals = 3)) # print() added
 qc = QuantumCircuit(q)
 qc.y(q)
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 print(job.result().get_unitary(qc, decimals = 3)) # print() added
@@ -70,7 +100,7 @@ print(job.result().get_unitary(qc, decimals = 3)) # print() added
 qc = QuantumCircuit(q)
 qc.z(q)
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 job.result().get_unitary(qc, decimals = 3)
@@ -80,41 +110,41 @@ job.result().get_unitary(qc, decimals = 3)
 qc = QuantumCircuit(q)
 qc.h(q)
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 print(job.result(). get_unitary(qc, decimals = 3)) # print() added
 
-# S (or sqrt(Z) phase) gate
+# S (또는 √Z phase) gate
 qc = QuantumCircuit(q)
 qc.s(q)
 qc.draw('mpl') # 'mpl' added
-plt.show()
+#plt.show()
 
 job = execute(qc, backend)
 print(job.result().get_unitary(qc, decimals = 3)) # print() added
 
-# S dagger (or conjugate of sqrt(Z) phase) gate
+# S† (또는 conjugate of √Z phase) 게이트
 qc = QuantumCircuit(q)
 qc.sdg(q)
 qc.draw('mpl')
-plt.show()
+#plt.show()
 
 # C3 gates --------------
-# T(or sqrt(S) phase) gate
+# T(또는 √S phase) gate
 qc = QuantumCircuit(q)
 qc.t(q)
 qc.draw('mpl') # 'mpl' added
-plt.show()
+#plt.show()
 
 job = execute(qc, backend)
 print(job.result().get_unitary(qc, decimals = 3)) # print() added
 
-# T(or conjugate of sqrt(S) phase) gate
+# T† (Or, conjugate of √S phase) gate
 qc = QuantumCircuit(q)
 qc.tdg(q)
 qc.draw('mpl')
-plt.show()
+#plt.show()
 
 job = execute(qc, backend)
 job.result().get_unitary(qc, decimals = 3)
@@ -124,7 +154,7 @@ job.result().get_unitary(qc, decimals = 3)
 qc = QuantumCircuit(q)
 qc.rx(pi / 2, q)
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 print(job.result().get_unitary(qc, decimals = 3)) # print() added
@@ -133,7 +163,7 @@ print(job.result().get_unitary(qc, decimals = 3)) # print() added
 qc = QuantumCircuit(q)
 qc.ry(pi / 2, q)
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 print(job.result().get_unitary(qc, decimals = 3)) # print() added
@@ -142,21 +172,21 @@ print(job.result().get_unitary(qc, decimals = 3)) # print() added
 qc = QuantumCircuit(q)
 qc.rz(pi / 2, q)
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 print(job.result().get_unitary(qc, decimals = 3)) # print() added
 
 # Multi-Qubit Gates -----------------------------------------------
 # Two-qubit gates------------------------
-q = QuantumRegister(2)
+q = QuantumRegister(2) 
 
 # Controlled Pauli Gates------
-# Controlled-X (or, controlled-NOT) gate
+# Controlled-X (Or, controlled-NOT) gate 
 qc = QuantumCircuit(q)
 qc.cx(q[0], q[1])
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 print('Controlled-X\n', job.result().get_unitary(qc, decimals = 3)) # print('Controlled-X\n',) added
@@ -165,7 +195,7 @@ print('Controlled-X\n', job.result().get_unitary(qc, decimals = 3)) # print('Con
 qc = QuantumCircuit(q)
 qc.cy(q[0], q[1])
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 print('Controlled-Y\n', job.result().get_unitary(qc, decimals = 3)) # print('Controlled-Y\n',) added
@@ -174,7 +204,7 @@ print('Controlled-Y\n', job.result().get_unitary(qc, decimals = 3)) # print('Con
 qc = QuantumCircuit(q)
 qc.cz(q[0], q[1])
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 print('Controlled-Z\n', job.result().get_unitary(qc, decimals = 3)) # print('Controlled-Z\n',) added
@@ -183,7 +213,7 @@ print('Controlled-Z\n', job.result().get_unitary(qc, decimals = 3)) # print('Con
 qc = QuantumCircuit(q)
 qc.ch(q[0], q[1])
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 print('Controlled-Hadamard\n', job.result().get_unitary(qc, decimals = 3)) # print('~~~',) added
@@ -193,7 +223,7 @@ print('Controlled-Hadamard\n', job.result().get_unitary(qc, decimals = 3)) # pri
 qc = QuantumCircuit(q)
 qc.crz(pi / 2, q[0], [1])
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 print('Controlled-Rotation of Z axis\n', job.result().get_unitary(qc, decimals = 3)) # print('~~~',) added
@@ -201,8 +231,14 @@ print('Controlled-Rotation of Z axis\n', job.result().get_unitary(qc, decimals =
 # Controlled phase rotation
 qc = QuantumCircuit(q)
 qc.cu1(pi / 2, q[0], q[1]) # deprecated
+'''
+The QuantumCircuit.cu1 method is deprecated as of 0.16.0. It will be removed no earlier than 
+3 months after the release date. You should use the QuantumCircuit.cp method instead, 
+which acts identically.
+qc.cu1(pi / 2, q[0], q[1])
+'''
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 print('Controlled phase rotation\n', job.result().get_unitary(qc, decimals = 3)) # print('~~~',) added
@@ -211,7 +247,12 @@ print('Controlled phase rotation\n', job.result().get_unitary(qc, decimals = 3))
 qc = QuantumCircuit(q)
 qc.cu3(pi / 2, pi / 2, pi / 2, q[0], q[1]) # deprecated
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
+'''
+The QuantumCircuit.cu3 method is deprecated as of 0.16.0. It will be removed no earlier than
+3 months after the release date. You should use the QuantumCircuit.cu method instead, where
+cu3(ϴ,φ,λ) = cu(ϴ,φ,λ,0).
+'''
 
 job = execute(qc, backend)
 print('Controlled u3 rotation\n', job.result().get_unitary(qc, decimals = 3)) # print('~~',) added
@@ -220,7 +261,7 @@ print('Controlled u3 rotation\n', job.result().get_unitary(qc, decimals = 3)) # 
 qc = QuantumCircuit(q)
 qc.swap(q[0], q[1])
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 print('Swap\n', job.result().get_unitary(qc, decimals = 3)) # print('~~',) added
@@ -232,7 +273,7 @@ q = QuantumRegister(3)
 qc = QuantumCircuit(q)
 qc.ccx(q[0], q[1], q[2])
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 print('Toffoli(CCX)\n', job.result().get_unitary(qc, decimals = 3)) # print('~~',) added
@@ -241,7 +282,7 @@ print('Toffoli(CCX)\n', job.result().get_unitary(qc, decimals = 3)) # print('~~'
 qc = QuantumCircuit(q)
 qc.cswap(q[0], q[1], q[2])
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend)
 print('Fredkin(CSWAP)\n', job.result().get_unitary(qc, decimals = 3)) # print('~~',) added
@@ -255,7 +296,7 @@ backend = BasicAer.get_backend('qasm_simulator')
 qc = QuantumCircuit(q, c)
 qc.measure(q, c)
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend, shots = 1024)
 print(job.result().get_counts(qc)) # print() added
@@ -264,7 +305,7 @@ qc = QuantumCircuit(q, c)
 qc.h(q) # 1-qubit
 qc.measure(q, c)
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend, shots=1024)
 print(job.result().get_counts(qc)) # print() added
@@ -274,7 +315,7 @@ qc = QuantumCircuit(q, c)
 qc.reset(q[0])
 qc.measure(q, c)
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend, shots = 1024)
 print(job.result().get_counts(qc)) # print() added, {'0': 1024}
@@ -284,7 +325,7 @@ qc.h(q)
 qc.reset(q[0])
 qc.measure(q, c)
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend, shots = 1024)
 print(job.result().get_counts(qc)) # print() added, {'0': 1024}
@@ -296,21 +337,21 @@ qc = QuantumCircuit(q, c)
 qc.x(q[0]).c_if(c, 0)
 qc.measure(q, c)
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend, shots = 1024)
 print(job.result().get_counts(qc)) # print() added, {'1': 1024}
 
 qc = QuantumCircuit(q, c)
 qc.h(q)
-qc.measure(q, c)
-qc.x(q[0]).c_if(c, 0)
+qc.measure(q, c) # 만약 qrk 0으로 측정되면, 다음 라인에서 1로 바뀜(측정 후에도 게이트 연산 가능).
+qc.x(q[0]).c_if(c, 0) # q가 1로 측정되면, 다음라인에서 안건드림
 qc.measure(q, c)
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 job = execute(qc, backend, shots = 1024)
-print(job.result().get_counts(qc)) # print() added
+print(job.result().get_counts(qc)) # print() added, 그래서 결론은 항상 |1>. {'1': 1024}
 
 # Arbitrary initialization
 # initializaing a three-qubit state
@@ -324,13 +365,13 @@ desired_vector = [
     1 / math.sqrt(8) * complex(1, 2),
     1 / math.sqrt(16) * complex(1, 0),
     0
-]
+] # 8개 = 2^3 개
 
 q = QuantumRegister(3)
 qc = QuantumCircuit(q)
 qc.initialize(desired_vector, [ q[0], q[1], q[2] ])
 qc.draw('mpl') # 'mpl' added
-plt.show() # added
+#plt.show() # added
 
 backend = BasicAer.get_backend('statevector_simulator')
 job = execute(qc, backend)
